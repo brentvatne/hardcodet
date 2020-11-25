2c37439e7062ba30c0cecb1e87c7391992fcd445
Brent Vatne <brentvatne@gmail.com>
2020-11-25 13:42:55 -0800

    Add code

--- App.js
+++ App.js
@@ -1,21 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { Button, Image, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+  const getKitten = async () => {
+    const result = await fetch("https://placekitten.com/300/300");
+    alert("result: " + result.status);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <Image
+        style={{ width: 100, height: 100 }}
+        source={{ uri: "https://placekitten.com/300/300" }}
+        resizeMode={"cover"}
+      />
+      <Button onPress={() => getKitten()} title={"get kitten"} />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
